1c1f339d3507a66a32fd5e9fde6c2b02c9527632
ksmin23 <sonagy23@gmail.com>
2024-07-31 15:23:19 +0900

    add web-analytics-on-aws project

---
+++ saas/web-analytics-on-aws/web_analytics/firehose.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+import random
+import string
+
+import aws_cdk as cdk
+
+from aws_cdk import (
+  Stack,
+  aws_iam,
+  aws_s3 as s3,
+  aws_kinesisfirehose,
+)
+
+from constructs import Construct
+from aws_cdk.aws_kinesisfirehose import CfnDeliveryStream as cfn
+
+random.seed(31)
+
+class FirehoseStack(Stack):
+
+  def __init__(self, scope: Construct, construct_id: str, source_kinesis_stream_arn, data_transform_lambda_fn, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
+
+    s3_bucket = s3.Bucket(self, "s3bucket",
+      removal_policy=cdk.RemovalPolicy.DESTROY, #XXX: Default: cdk.RemovalPolicy.RETAIN - The bucket will be orphaned
+      bucket_name="web-analytics-{region}-{account_id}".format(
+        region=cdk.Aws.REGION, account_id=cdk.Aws.ACCOUNT_ID))
+
+    FIREHOSE_DEFAULT_STREAM_NAME = 'PUT-S3-{}'.format(''.join(random.sample((string.ascii_letters), k=5)))
+    firehose_config = self.node.try_get_context('firehose')
+
+    FIREHOSE_STREAM_NAME = firehose_config.get('stream_name', FIREHOSE_DEFAULT_STREAM_NAME)
+    FIREHOSE_BUFFER_SIZE = firehose_config['buffer_size_in_mbs']
+    FIREHOSE_BUFFER_INTERVAL = firehose_config['buffer_interval_in_seconds']
+    FIREHOSE_LAMBDA_BUFFER_SIZE = firehose_config['lambda_buffer_size_in_mbs']
+    FIREHOSE_LAMBDA_BUFFER_INTERVAL = firehose_config['lambda_buffer_interval_in_seconds']
+    FIREHOSE_LAMBDA_NUMBER_OF_RETRIES = firehose_config['lambda_number_of_retries']
+    FIREHOSE_TO_S3_PREFIX = firehose_config['prefix']
+    FIREHOSE_TO_S3_ERROR_OUTPUT_PREFIX = firehose_config['error_output_prefix']
+    FIREHOSE_TO_S3_OUTPUT_FOLDER = firehose_config['s3_output_folder']
+
+    assert f'{FIREHOSE_TO_S3_OUTPUT_FOLDER}/' == FIREHOSE_TO_S3_PREFIX[:len(FIREHOSE_TO_S3_OUTPUT_FOLDER) + 1]
+
+    firehose_role_policy_doc = aws_iam.PolicyDocument()
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(**{
+      "effect": aws_iam.Effect.ALLOW,
+      "resources": [s3_bucket.bucket_arn, "{}/*".format(s3_bucket.bucket_arn)],
+      "actions": ["s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject"]
+    }))
+
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["ec2:DescribeVpcs",
+        "ec2:DescribeVpcAttribute",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:CreateNetworkInterface",
+        "ec2:CreateNetworkInterfacePermission",
+        "ec2:DeleteNetworkInterface"]
+    ))
+
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["glue:GetTable",
+        "glue:GetTableVersion",
+        "glue:GetTableVersions"]
+    ))
+
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=[source_kinesis_stream_arn],
+      actions=["kinesis:DescribeStream",
+        "kinesis:GetShardIterator",
+        "kinesis:GetRecords"]
+    ))
+
+    firehose_log_group_name = f"/aws/kinesisfirehose/{FIREHOSE_STREAM_NAME}"
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      #XXX: The ARN will be formatted as follows:
+      # arn:{partition}:{service}:{region}:{account}:{resource}{sep}}{resource-name}
+      resources=[self.format_arn(service="logs", resource="log-group",
+        resource_name="{}:log-stream:*".format(firehose_log_group_name),
+        arn_format=cdk.ArnFormat.COLON_RESOURCE_NAME)],
+      actions=["logs:PutLogEvents"]
+    ))
+
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(**{
+      "effect": aws_iam.Effect.ALLOW,
+      #XXX: The ARN will be formatted as follows:
+      # arn:{partition}:{service}:{region}:{account}:{resource}{sep}}{resource-name}
+      "resources": [self.format_arn(partition="aws", service="lambda",
+        region=cdk.Aws.REGION, account=cdk.Aws.ACCOUNT_ID, resource="function",
+        resource_name="{}:*".format(data_transform_lambda_fn.function_name),
+        arn_format=cdk.ArnFormat.COLON_RESOURCE_NAME)],
+      "actions": ["lambda:InvokeFunction",
+        "lambda:GetFunctionConfiguration"]
+    }))
+
+    firehose_role = aws_iam.Role(self, "KinesisFirehoseDeliveryRole",
+      role_name="KinesisFirehoseServiceRole-{stream_name}-{region}".format(
+        stream_name=FIREHOSE_STREAM_NAME, region=cdk.Aws.REGION),
+      assumed_by=aws_iam.ServicePrincipal("firehose.amazonaws.com"),
+      #XXX: use inline_policies to work around https://github.com/aws/aws-cdk/issues/5221
+      inline_policies={
+        "firehose_role_policy": firehose_role_policy_doc
+      }
+    )
+
+    lambda_proc = cfn.ProcessorProperty(
+      type="Lambda",
+      parameters=[
+        cfn.ProcessorParameterProperty(
+          parameter_name="LambdaArn",
+          # parameter_value='{}:{}'.format(schema_validator_lambda_fn.function_arn, schema_validator_lambda_fn.current_version.version)
+          parameter_value='{}:{}'.format(data_transform_lambda_fn.function_arn, data_transform_lambda_fn.latest_version.version)
+        ),
+        cfn.ProcessorParameterProperty(
+          parameter_name="NumberOfRetries",
+          parameter_value=str(FIREHOSE_LAMBDA_NUMBER_OF_RETRIES)
+        ),
+        cfn.ProcessorParameterProperty(
+          parameter_name="RoleArn",
+          parameter_value=firehose_role.role_arn
+        ),
+        cfn.ProcessorParameterProperty(
+          parameter_name="BufferSizeInMBs",
+          parameter_value=str(FIREHOSE_LAMBDA_BUFFER_SIZE)
+        ),
+        cfn.ProcessorParameterProperty(
+          parameter_name="BufferIntervalInSeconds",
+          parameter_value=str(FIREHOSE_LAMBDA_BUFFER_INTERVAL)
+        )
+      ]
+    )
+
+    firehose_processing_config = cfn.ProcessingConfigurationProperty(
+      enabled=True,
+      processors=[
+        lambda_proc
+      ]
+    )
+
+    ext_s3_dest_config = cfn.ExtendedS3DestinationConfigurationProperty(
+      bucket_arn=s3_bucket.bucket_arn,
+      role_arn=firehose_role.role_arn,
+      buffering_hints={
+        "intervalInSeconds": FIREHOSE_BUFFER_INTERVAL,
+        "sizeInMBs": FIREHOSE_BUFFER_SIZE
+      },
+      cloud_watch_logging_options={
+        "enabled": True,
+        "logGroupName": firehose_log_group_name,
+        "logStreamName": "S3Delivery"
+      },
+      compression_format="UNCOMPRESSED", # [GZIP | HADOOP_SNAPPY | Snappy | UNCOMPRESSED | ZIP]
+      data_format_conversion_configuration={
+        "enabled": False
+      },
+      dynamic_partitioning_configuration={
+        "enabled": False
+      },
+      error_output_prefix=FIREHOSE_TO_S3_ERROR_OUTPUT_PREFIX,
+      prefix=FIREHOSE_TO_S3_PREFIX,
+      processing_configuration=firehose_processing_config
+    )
+
+    firehose_to_s3_delivery_stream = aws_kinesisfirehose.CfnDeliveryStream(self, "KinesisFirehoseToS3",
+      delivery_stream_name=FIREHOSE_STREAM_NAME,
+      delivery_stream_type="KinesisStreamAsSource",
+      kinesis_stream_source_configuration={
+        "kinesisStreamArn": source_kinesis_stream_arn,
+        "roleArn": firehose_role.role_arn
+      },
+      extended_s3_destination_configuration=ext_s3_dest_config
+    )
+
+    self.s3_dest_bucket_name = s3_bucket.bucket_name
+    self.s3_dest_folder_name = FIREHOSE_TO_S3_OUTPUT_FOLDER
+
+    cdk.CfnOutput(self, '{}_S3DestBucket'.format(self.stack_name), value=s3_bucket.bucket_name, export_name='S3DestBucket')
+    cdk.CfnOutput(self, '{}_KinesisDataFirehoseName'.format(self.stack_name),
+      value=firehose_to_s3_delivery_stream.delivery_stream_name, export_name='KinesisDataFirehoseName')
+
---
+++ saas/web-analytics-on-aws/web_analytics/merge_small_files_lambda.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# vim: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
+
+import os
+
+import aws_cdk as cdk
+
+from aws_cdk import (
+  Stack,
+  aws_iam,
+  aws_lambda,
+  aws_logs,
+  aws_events,
+  aws_events_targets
+)
+from constructs import Construct
+
+class MergeSmallFilesLambdaStack(Stack):
+
+  def __init__(self, scope: Construct, construct_id: str, s3_bucket_name, s3_folder_name, athena_work_group, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
+
+    _lambda_env = self.node.try_get_context('merge_small_files_lambda_env')
+
+    LAMBDA_ENV_VARS = [
+      'OLD_DATABASE',
+      'OLD_TABLE_NAME',
+      'NEW_DATABASE',
+      'NEW_TABLE_NAME',
+      'ATHENA_WORK_GROUP',
+      'OLD_TABLE_LOCATION_PREFIX',
+      'OUTPUT_PREFIX',
+      'STAGING_OUTPUT_PREFIX',
+      'COLUMN_NAMES'
+    ]
+
+    lambda_fn_env = {k: v for k, v in _lambda_env.items() if k in LAMBDA_ENV_VARS}
+    additional_lambda_fn_env = {
+      'ATHENA_WORK_GROUP': athena_work_group,
+      'OLD_TABLE_LOCATION_PREFIX': f"s3://{os.path.join(s3_bucket_name, s3_folder_name)}",
+      'OUTPUT_PREFIX': f"s3://{os.path.join(s3_bucket_name, _lambda_env['NEW_TABLE_S3_FOLDER_NAME'])}",
+      'STAGING_OUTPUT_PREFIX': f"s3://{os.path.join(s3_bucket_name, 'tmp')}",
+      'REGION_NAME': cdk.Aws.REGION
+    }
+    lambda_fn_env.update(additional_lambda_fn_env)
+
+    self.s3_json_location, self.s3_parquet_location = (lambda_fn_env['OLD_TABLE_LOCATION_PREFIX'], lambda_fn_env['OUTPUT_PREFIX'])
+
+    merge_small_files_lambda_fn = aws_lambda.Function(self, "MergeSmallFiles",
+      runtime=aws_lambda.Runtime.PYTHON_3_11,
+      function_name="MergeSmallFiles",
+      handler="athena_ctas.lambda_handler",
+      description="Merge small files in S3",
+      code=aws_lambda.Code.from_asset('./src/main/python/MergeSmallFiles'),
+      environment=lambda_fn_env,
+      timeout=cdk.Duration.minutes(5)
+    )
+
+    merge_small_files_lambda_fn.add_to_role_policy(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["athena:*"]))
+
+    merge_small_files_lambda_fn.add_to_role_policy(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["s3:Get*",
+        "s3:List*",
+        "s3:AbortMultipartUpload",
+        "s3:PutObject",
+      ]))
+
+    merge_small_files_lambda_fn.add_to_role_policy(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["glue:CreateDatabase",
+        "glue:DeleteDatabase",
+        "glue:GetDatabase",
+        "glue:GetDatabases",
+        "glue:UpdateDatabase",
+        "glue:CreateTable",
+        "glue:DeleteTable",
+        "glue:BatchDeleteTable",
+        "glue:UpdateTable",
+        "glue:GetTable",
+        "glue:GetTables",
+        "glue:BatchCreatePartition",
+        "glue:CreatePartition",
+        "glue:DeletePartition",
+        "glue:BatchDeletePartition",
+        "glue:UpdatePartition",
+        "glue:GetPartition",
+        "glue:GetPartitions",
+        "glue:BatchGetPartition"
+      ]))
+
+    merge_small_files_lambda_fn.add_to_role_policy(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["lakeformation:GetDataAccess"]))
+
+    lambda_fn_target = aws_events_targets.LambdaFunction(merge_small_files_lambda_fn)
+    aws_events.Rule(self, "ScheduleRule",
+      schedule=aws_events.Schedule.cron(minute="10"),
+      targets=[lambda_fn_target]
+    )
+
+    log_group = aws_logs.LogGroup(self, "MergeSmallFilesLogGroup",
+      log_group_name=f"/aws/lambda/{self.stack_name}/MergeSmallFiles",
+      removal_policy=cdk.RemovalPolicy.DESTROY, #XXX: for testing
+      retention=aws_logs.RetentionDays.THREE_DAYS)
+    log_group.grant_write(merge_small_files_lambda_fn)
+
+    self.lambda_exec_role = merge_small_files_lambda_fn.role
+
+    cdk.CfnOutput(self, f'{self.stack_name}_MergeFilesFuncName',
+      value=merge_small_files_lambda_fn.function_name,
+      export_name='MergeFilesLambdaFuncName')
+
+    cdk.CfnOutput(self, f'{self.stack_name}_LambdaExecRoleArn',
+      value=self.lambda_exec_role.role_arn)
