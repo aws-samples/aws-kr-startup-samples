f6c23fbc05733c2164fb966f43b4d25e23c9c35f
tjdwlsdlaek <asj4514@naver.com>
2025-07-29 19:04:26 +0900

    Remove Wan2.1 /gen-ai

--- machine-learning/sagemaker/wan21-on-sagemaker/cdk_stacks/sm_endpoint_config.py
+++ machine-learning/sagemaker/wan21-on-sagemaker/cdk_stacks/sm_endpoint_config.py
@@ -3,24 +3,25 @@ from aws_cdk import (
     aws_sagemaker as sagemaker,
     Stack,
 )
-
 from constructs import Construct
 
 class SageMakerEndpointConfigStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, model, s3_output_bucket, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # SageMaker Endpoint Configuration
         self.endpoint_config = sagemaker.CfnEndpointConfig(
-            self, "WanVideoRealtimeEndpointConfig",
+            self, "WanVideoRealtimeEndpointConfig", # Changed resource name
             production_variants=[sagemaker.CfnEndpointConfig.ProductionVariantProperty(
                 initial_instance_count=1,
                 initial_variant_weight=1.0,
-                instance_type="ml.g6.2xlarge",
-                model_name=cdk.Fn.import_value("WanVideoSageMakerModelName"),
+                instance_type="ml.g6.2xlarge",  # Changed to requested type
+                model_name=model.attr_model_name,  # 원래처럼 직접 사용
                 variant_name="AllTraffic"
             )]
         )
 
+        # Export Endpoint Configuration name
         cdk.CfnOutput(
             self, "EndpointConfigName",
             value=self.endpoint_config.attr_endpoint_config_name,
