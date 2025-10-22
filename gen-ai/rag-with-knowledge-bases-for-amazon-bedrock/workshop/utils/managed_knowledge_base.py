acd3987266876f9dd76aa55f8a7b303375b6908b
YongHwan Yoo <yhyoo@amazon.com>
2025-10-22 09:11:17 +0900

    update utils folder

--- gen-ai/rag-with-knowledge-bases-for-amazon-bedrock/workshop/utils/managed_knowledge_base.py
+++ gen-ai/rag-with-knowledge-bases-for-amazon-bedrock/workshop/utils/managed_knowledge_base.py
@@ -14,7 +14,8 @@ valid_generation_models = ["anthropic.claude-3-5-sonnet-20240620-v1:0",
                           "anthropic.claude-3-5-haiku-20241022-v1:0", 
                           "anthropic.claude-3-sonnet-20240229-v1:0",
                           "anthropic.claude-3-haiku-20240307-v1:0",
-                          "amazon.nova-micro-v1:0"] 
+                          "amazon.nova-micro-v1:0",
+                          "amazon.nova-pro-v1:0"] 
 
 
 pp = pprint.PrettyPrinter(indent=2)
